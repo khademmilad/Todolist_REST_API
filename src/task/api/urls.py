3f14b8f987f7080b3de6fc59e4ce6c26c67c0ddb
khademmilad <khadmemilad@gmail.com>
2021-11-10 10:18:47 +0100

    add PUT DELETE POST api

--- src/task/api/urls.py
+++ src/task/api/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 from task.api.views import (
     task_list,
+    api_task_detail,
+    api_task_update,
+    api_task_delete,
+    api_task_create
 )
 
 urlpatterns = [
-    path('tasks/', task_list, name='task_list')
+    path('tasks/', task_list, name='task_list'),
+    path('task/<int:id>', api_task_detail, name='task-detail'),
+    path('task/<int:pk>/update', api_task_update, name="task-update"),
+    path('task/<int:pk>/delete', api_task_delete, name="task-delete"),
+    path('task/create', api_task_create, name='task-create')
 ]
